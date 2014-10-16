845605a916a56159839f4df940830f4dfccdc4cc
Etienne Obriot <etienne.obriot@gmail.com>
2014-10-16 19:13:32 +0200

    Initial commit

---
+++ data/logic.js
@@ -0,0 +1,26 @@
+var div_tab; 
+self.on('click', function() {
+   self.postMessage('clicked');
+});
+
+self.port.on('loadPanel', function(tab_list) {
+
+   //console.log('Load Panel');
+   //console.log(tab_list);
+   div_tab = document.getElementById('tablist'); 
+   div_tab.innerHTML = "";
+   list_element = document.createElement("ul");
+   for each (tab in tab_list) {
+      item = document.createElement("li");
+      text_node = document.createTextNode(tab.title);
+      item.setAttribute('data-index', tab.index);
+      item.addEventListener('click', tab_clicked.bind(item));
+      item.appendChild(text_node);
+      list_element.appendChild(item);
+   }
+   div_tab.appendChild(list_element);
+});
+
+function tab_clicked(event) {
+   self.port.emit('tab_clicked', this.dataset.index);
+}
